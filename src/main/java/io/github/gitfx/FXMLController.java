d09da187f3ff51c43ce611b8caddb2d991675fa9
rohit <rohitvvv@gmail.com>
2015-01-05 01:35:54 +0530

    Scene.fxml -> Added layout components to FXML. MainApp.java -> Changed application Title FXMLController -> Commented bare bornes template code

--- src/main/java/io/github/gitfx/FXMLController.java
+++ src/main/java/io/github/gitfx/FXMLController.java
@@ -6,17 +6,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class FXMLController implements Initializable {
     
-    @FXML
+    /*@FXML
     private Label label;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
-    }
+    }*/
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
