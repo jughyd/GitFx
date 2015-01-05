58148ea358bd03cf468f78c70e02e77bdc464fff
Rajmahendra Hegde <rajmahendra@gmail.com>
2015-01-05 17:02:33 +0530

    added button handlers and license

--- src/main/java/io/github/gitfx/controller/GitFxController.java
+++ src/main/java/io/github/gitfx/controller/GitFxController.java
@@ -1,16 +1,30 @@
-package io.github.gitfx;
+/**
+ * Copyright 2015 GitFx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.gitfx.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class FXMLController implements Initializable {
+public class GitFxController implements Initializable {
     
    
     @FXML 
@@ -46,4 +60,30 @@ public class FXMLController implements Initializable {
         Image gitInitGraphic = new Image(getClass().getResourceAsStream("/icons/git.gif"));
         gitinit.setGraphic(new ImageView(gitInitGraphic));
     }    
+    
+    
+    @FXML
+    public void onGitSettingsClicked(ActionEvent event){
+    }
+    
+    
+    @FXML
+    public void onGitCloneClicked(ActionEvent event){
+    }
+    
+    @FXML
+    public void onGitOpenClicked(ActionEvent event){
+    }
+    
+    @FXML
+    public void onGitSyncClicked(ActionEvent event){
+    }
+    
+    @FXML
+    public void onGitInitClicked(ActionEvent event){
+    }
+    
+    
+    
+    
 }
