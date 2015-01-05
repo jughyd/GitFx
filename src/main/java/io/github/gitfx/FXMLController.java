81cb538517388b5b8929c0f2bfddf10657533dbd
rohit <rohitvvv@gmail.com>
2015-01-05 08:50:57 +0530

    Adding new Icons files under resources Added code in the Controller to pick up the icons

--- src/main/java/io/github/gitfx/FXMLController.java
+++ src/main/java/io/github/gitfx/FXMLController.java
@@ -7,8 +7,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FXMLController implements Initializable {
+    
+   
+    @FXML 
+    private Button gitclone;
+    @FXML 
+    private Button gitsettings;
+    @FXML
+    private Button gitsync;
+    @FXML
+    private Button gitinit;
+    @FXML
+    private Button gitopen;
     
     /*@FXML
     private Label label;
@@ -21,6 +35,15 @@ public class FXMLController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Image gitCloneGraphic = new Image(getClass().getResourceAsStream("/icons/clone.gif"));
+        gitclone.setGraphic(new ImageView(gitCloneGraphic));
+        Image gitSettingsGraphic = new Image(getClass().getResourceAsStream("/icons/settings.gif"));
+        gitsettings.setGraphic(new ImageView(gitSettingsGraphic));
+        Image gitSyncGraphic = new Image(getClass().getResourceAsStream("/icons/sync.gif"));
+        gitsync.setGraphic(new ImageView(gitSyncGraphic));
+        Image gitOpenGraphic = new Image(getClass().getResourceAsStream("/icons/open.gif"));
+        gitopen.setGraphic(new ImageView(gitOpenGraphic));
+        Image gitInitGraphic = new Image(getClass().getResourceAsStream("/icons/git.gif"));
+        gitinit.setGraphic(new ImageView(gitInitGraphic));
     }    
 }
