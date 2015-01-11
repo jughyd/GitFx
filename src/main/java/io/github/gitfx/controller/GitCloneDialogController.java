b48db615a971810d64dd4142f56b11b2f753418b
rohit <rohitvvv@gmail.com>
2015-01-11 19:50:14 +0530

    Linking all Dialogs to menu controls. Good starting point for jgit developers to start developing from this point.

---
+++ src/main/java/io/github/gitfx/controller/GitCloneDialogController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.gitfx.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author rvvaidya
+ */
+public class GitCloneDialogController implements Initializable {
+    @FXML
+    private AnchorPane openDialog;
+    @FXML
+    private Button chooseRepo;
+    @FXML
+    private Button ok;
+    @FXML
+    private Button cancel;
+    
+    private Stage dialogStage;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onCancelClicked(ActionEvent event) {
+       dialogStage.close();
+    }
+    
+    /**
+     * Sets the stage of this dialog.
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+    
+}
